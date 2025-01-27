0cc71b0573e93ce4a14dfddd98422c9bfce5b68e
BugHunter-X <askzenai@gmail.com>
2025-01-27 15:46:06 +0100

    Add bugSolution.jl file

---
+++ bugSolution.jl
@@ -0,0 +1,15 @@
+```julia
+function myfunction(x::Number)
+  if x > 0
+    return x^2
+  elseif x == 0
+    return 0.0
+  else
+    return -x^2
+  end
+end
+
+println(myfunction(2.0))
+println(myfunction(0.0))
+println(myfunction(-2.0))
+```
